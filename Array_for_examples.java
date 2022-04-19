6e34176d6b01d936cf89511c989c735da0c01444
GurutheKing <guru.sgr@gmail.com>
2022-04-19 17:30:45 +0530

    test

---
+++ Array_for_examples.java
@@ -0,0 +1,23 @@
+public class Array_for_examples {
+
+	public static void main(String[] args) {
+		HashMap hm = new HashMap();
+		hm.put("Tea", 10); 
+		hm.put("Coffee", 10); 
+		hm.put("Tiffin", 30); 
+		hm.put("Meals", 100); 
+		hm.put("Dinner", 40); 
+		System.out.println(hm); 
+		// TODO Auto-generated method stub
+		//int[] ar = { 10, 20, 30 };
+
+//		for (int no : ar) {
+//			System.out.println(no);
+//		}
+//		for(Object obj:al)
+//		{
+//			System.out.println(al);
+//		} 
+	}
+
+}
